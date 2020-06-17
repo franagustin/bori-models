87e4b84e1a38232efc0f73afbc5b681359cb8a87
Fran Agustín <francokuchiki@gmail.com>
2020-06-17 10:28:05 -0300

    Add method to list valid settings

--- bori_models/models.py
+++ bori_models/models.py
@@ -1,3 +1,5 @@
+from typing import Dict, Tuple
+
 from schematics import types
 from schematics.models import Model
 
@@ -13,11 +15,28 @@ class MongoModel(Model):
 
 
 class GuildSettings(MongoModel):
+    exclude_fields = ('_id', 'guild_id')
+
     guild_id = types.IntType(required=True)
     disabled_cogs = types.ListType(types.StringType)
     disabled_commands = types.ListType(types.StringType)
     muted_role = types.IntType()
     prefixes = types.ListType(types.StringType)
+
+    @classmethod
+    def valid_settings(cls) -> Tuple[Dict[str, bool], Dict[str, str]]:
+        valid_settings, aliases = {}, {}
+        for field, field_type in cls._fields.items():  # pylint: disable=no-member
+            if field in cls.exclude_fields:
+                continue
+            is_multiple = field_type.native_type is list
+            valid_settings[field] = is_multiple
+            if is_multiple and field.endswith('s'):
+                finish = -2 if field.endswith('es') else -1
+                alias = field[:finish]
+                aliases[alias] = field
+                valid_settings[alias] = is_multiple
+        return valid_settings, aliases
 
 
 class MutedMember(MongoModel):
